c463a53a479226ea57eab6306813004fc257e57e
subnixr <mariani.roberto94@gmail.com>
2025-06-07 19:16:55 +0100

    implement curriculum, import data

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,27 @@
 import Icon from '@/components/base/Icon';
+import { fileExists, readCV } from '@/utils/cv';
 import { LOCALES } from '@/utils/i18n/config';
 import { initI18N, readTranslations } from '@/utils/i18n/server';
 
 export async function generateStaticParams() {
-    return LOCALES.map(locale => ({ locale }));
+    const res = await Promise.all(
+        ([...LOCALES] as string[]).map(async locale => {
+            const exists = await fileExists(
+                `src/app/[locale]/data/${locale}.yml`,
+            );
+            return !exists ? null : { locale };
+        }),
+    );
+    return res.filter(Boolean);
 }
 
 export async function generateMetadata({ params }) {
     const { locale } = await params;
-    return { lang: locale };
+    const data = await readCV(`src/app/[locale]/data/${locale}.yml`);
+    return {
+        ...data.seo,
+        lang: locale,
+    };
 }
 
 type Params = { locale: string };
@@ -20,13 +33,17 @@ export type PageProps = Readonly<{
 export default async function Page({ params }: PageProps) {
     const { locale } = await params;
 
-    const translations = await readTranslations([...LOCALES]);
+    const [translations, cv] = await Promise.all([
+        readTranslations([...LOCALES]),
+        readCV(`src/app/[locale]/data/${locale}.yml`),
+    ]);
 
     const t = initI18N(translations, locale);
 
     return (
         <div>
             {t('common.helloWorld')}
+            {cv.profile.name}
             <Icon type="sun" className="icon-300 inline-block" />
         </div>
     );
---
+++ src/utils/cv.ts
@@ -0,0 +1,84 @@
+import fs from 'fs/promises';
+import yaml from 'js-yaml';
+import path from 'path';
+
+export type Profile = {
+    name: string;
+    title: string;
+    address: string;
+    image: {
+        desktop: string;
+        mobile: string;
+    };
+    imageAlt: string;
+    linkedin: string;
+    github: string;
+};
+
+export type SEO = {
+    author: string;
+    description: string;
+    keywords: string[];
+    opengraph: {
+        title: string;
+        type: string;
+        image: string;
+    };
+};
+
+export type Skill = {
+    label: string;
+    rank?: string;
+    url?: string;
+};
+
+export type Language = {
+    name: string;
+    listen: string;
+    speak: string;
+    read: string;
+    write: string;
+};
+
+export type Experience = {
+    role: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+
+    entity?: string;
+    location?: string;
+    url?: string;
+    linkedin?: string;
+    thumbnail?: string;
+    thumbnailAlt?: string;
+};
+
+export type Curriculum = {
+    profile: Profile;
+    skills: {
+        hard: Skill[];
+        soft: Skill[];
+        hobbies: Skill[];
+    };
+    languages: Language[];
+    seo: SEO;
+    experiences: Experience[];
+    education: Experience[];
+};
+
+export async function readCV(filepath: string): Promise<Curriculum> {
+    const p = path.join(process.cwd(), filepath);
+    const content = await fs.readFile(p, 'utf-8');
+    return yaml.load(content) as Curriculum;
+}
+
+export async function fileExists(filepath: string): Promise<boolean> {
+    const p = path.join(process.cwd(), filepath);
+    try {
+        await fs.access(p);
+        return true;
+    } catch {
+        return false;
+    }
+}

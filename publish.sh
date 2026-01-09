7faaccf719d187e27f19b1db8aae6f5206dfa920
subnixr <subnixr@gmx.com>
2026-01-09 20:57:00 +0000

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 npm run build
+if [ ! -d ./subnixr.github.io ]; then 
+    git clone git@github.com:subnixr/subnixr.github.io
+fi
 rm -r ./subnixr.github.io/*
 cp -r ./out/* ./subnixr.github.io
 cd subnixr.github.io/
